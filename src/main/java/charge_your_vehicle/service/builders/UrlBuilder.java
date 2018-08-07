1e2c729bbc5dfbf2df422c03152ebb316c45230a
JWatus <jwatus@pl.sii.eu>
2018-08-07 09:19:27 +0200

    Added URL builder

---
+++ src/main/java/charge_your_vehicle/service/builders/UrlBuilder.java
@@ -0,0 +1,23 @@
+package charge_your_vehicle.service.builders;
+
+public class UrlBuilder {
+    private String url;
+
+    public UrlBuilder(String url) {
+        this.url = url;
+    }
+
+    public UrlBuilder addParameterToUrl(String key, String value) {
+        url += (url.contains("?")) ? "&" : "?";
+        url += key;
+        url += "=";
+        url += value;
+
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return url;
+    }
+}

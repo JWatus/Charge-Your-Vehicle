f5a3e54203c44a32bdbd259274ee830df7c81424
JWatus <jwatus@pl.sii.eu>
2018-08-09 12:47:07 +0200

    FindTheClosest charging point template and controller added

--- src/main/java/charge_your_vehicle/view/controllers/FindTheClosestController.java
+++ src/main/java/charge_your_vehicle/view/controllers/FindTheClosestController.java
@@ -1,34 +1,32 @@
-package charge_your_vehicle.view.servlets;
+package charge_your_vehicle.view.controllers;
 
 import charge_your_vehicle.dao.ChargingPointDao;
 import charge_your_vehicle.dto.ChargingPointDto;
 import charge_your_vehicle.model.ChargingPoint;
-import charge_your_vehicle.model.User;
 import charge_your_vehicle.service.converters.CoordinatesConverter;
 import charge_your_vehicle.service.data_filters.DataFilter;
 import charge_your_vehicle.service.promoted.ChargingPointToDtoConverterBean;
 import charge_your_vehicle.service.properties.AppPropertiesBean;
 import charge_your_vehicle.view.commons.Formaters;
 import charge_your_vehicle.view.freemarker.TemplateProvider;
+import charge_your_vehicle.view.servlets.FindTheClosestInRadiusServlet;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@WebServlet("/find-the-closest2")
-public class FindTheClosestServlet extends HttpServlet {
+@Controller
+public class FindTheClosestController {
     public static final Logger LOG = LoggerFactory.getLogger(FindTheClosestInRadiusServlet.class);
 
     private ChargingPointDao chargingPointDao;
@@ -37,11 +35,11 @@ public class FindTheClosestServlet extends HttpServlet {
     private ChargingPointToDtoConverterBean chargingPointToDtoConverterBean;
     private AppPropertiesBean appPropertiesBean;
 
-    public FindTheClosestServlet(ChargingPointDao chargingPointDao,
-                                 DataFilter dataFilter,
-                                 CoordinatesConverter coordinatesConverter,
-                                 ChargingPointToDtoConverterBean chargingPointToDtoConverterBean,
-                                 AppPropertiesBean appPropertiesBean) {
+    public FindTheClosestController(ChargingPointDao chargingPointDao,
+                                    DataFilter dataFilter,
+                                    CoordinatesConverter coordinatesConverter,
+                                    ChargingPointToDtoConverterBean chargingPointToDtoConverterBean,
+                                    AppPropertiesBean appPropertiesBean) {
         this.chargingPointDao = chargingPointDao;
         this.dataFilter = dataFilter;
         this.coordinatesConverter = coordinatesConverter;
@@ -49,32 +47,22 @@ public class FindTheClosestServlet extends HttpServlet {
         this.appPropertiesBean = appPropertiesBean;
     }
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    @RequestMapping(value = "/find-the-closest", method = RequestMethod.GET)
+    public ModelAndView getFindTheClosestPage(HttpSession session) {
 
         LOG.info("User searched closest charging station");
 
-        Map<String, Object> dataModel = new HashMap<>();
-        dataModel.put("title", "Find the closest charging point");
+        ModelAndView modelAndView = new ModelAndView("body-templates/find-the-closest");
+        modelAndView.addObject("title", "Find the closest charging point");
 
-        Object userObject = req.getSession().getAttribute("user");
-        User user;
-        if (userObject != null) {
-            user = (User) userObject;
-            dataModel.put("userSessionName", user.getName());
-            dataModel.put("userAdmin", user.getRoleAdministration());
-        }
-
-        PrintWriter writer = resp.getWriter();
-        resp.setContentType("text/html;charset=UTF-8");
-        String directionLong = req.getParameter("directionLong");
-        String degreesLong = req.getParameter("degreesLong");
-        String minutesLong = req.getParameter("minutesLong");
-        String secondsLong = req.getParameter("secondLong");
-        String directionLati = req.getParameter("directionLati");
-        String degreesLati = req.getParameter("degreesLati");
-        String minutesLati = req.getParameter("minutesLati");
-        String secondsLati = req.getParameter("secondLati");
+        String directionLong = (String) session.getAttribute("directionLong");
+        String degreesLong = (String) session.getAttribute("degreesLong");
+        String minutesLong = (String) session.getAttribute("minutesLong");
+        String secondsLong = (String) session.getAttribute("secondLong");
+        String directionLati = (String) session.getAttribute("directionLati");
+        String degreesLati = (String) session.getAttribute("degreesLati");
+        String minutesLati = (String) session.getAttribute("minutesLati");
+        String secondsLati = (String) session.getAttribute("secondLati");
 
         boolean isDegreesLongNull = (degreesLong == null || degreesLong.isEmpty());
         boolean isMinutesLongNull = (minutesLong == null || minutesLong.isEmpty());
@@ -85,12 +73,12 @@ public class FindTheClosestServlet extends HttpServlet {
 
         if ((isDegreesLongNull && isMinutesLongNull && isSecondsLongNull)
                 && (isDegreesLatiNull && isMinutesLatiNull && isSecondsLatiNull)) {
-            dataModel.put("body_template", "find-the-closest");
-            dataModel.put("current_unit", Formaters.naturalFormat(appPropertiesBean.getCurrentUnit().name()));
-            dataModel.put("title", "Find the closest charging point");
+            modelAndView.addObject("body_template", "find-the-closest");
+            modelAndView.addObject("current_unit", Formaters.naturalFormat(appPropertiesBean.getCurrentUnit().name()));
+            modelAndView.addObject("title", "Find the closest charging point");
         } else if ((isDegreesLongNull && isMinutesLongNull && isSecondsLongNull)
                 || (isDegreesLatiNull && isMinutesLatiNull && isSecondsLatiNull)) {
-            errorMessages(dataModel);
+            errorMessages(modelAndView);
         } else {
             if (isDegreesLongNull) degreesLong = "0";
             if (isMinutesLongNull) minutesLong = "0";
@@ -114,36 +102,28 @@ public class FindTheClosestServlet extends HttpServlet {
                                     latitude);
                     chargingPointsList.add(chargingPoint);
                     List<ChargingPointDto> chargingPointsDtoList = chargingPointToDtoConverterBean.convertList(chargingPointsList);
-                    dataModel.put("body_template", "results");
-                    dataModel.put("chargingPoints", chargingPointsDtoList);
-                    dataModel.put("latitude", latitude);
-                    dataModel.put("longitude", longitude);
-                    dataModel.put("google_api_key", appPropertiesBean.getGoogleApiKey());
+                    modelAndView.addObject("body_template", "results");
+                    modelAndView.addObject("chargingPoints", chargingPointsDtoList);
+                    modelAndView.addObject("latitude", latitude);
+                    modelAndView.addObject("longitude", longitude);
+                    modelAndView.addObject("google_api_key", appPropertiesBean.getGoogleApiKey());
                 } catch (Exception e) {
-                    dataModel.put("body_template", "find-the-closest");
-                    dataModel.put("title", "Find the closest charging point");
-                    dataModel.put("error", "No charging points were found");
+                    modelAndView.addObject("body_template", "find-the-closest");
+                    modelAndView.addObject("title", "Find the closest charging point");
+                    modelAndView.addObject("error", "No charging points were found");
                     LOG.error("No charging points were found");
                 }
             } else {
-                errorMessages(dataModel);
+                errorMessages(modelAndView);
             }
         }
-        resp.setContentType("text/html;charset=UTF-8");
-
-        Template template = TemplateProvider.createTemplate(getServletContext(), "templates/body-templates/home.html");
-
-        try {
-            template.process(dataModel, writer);
-        } catch (TemplateException e) {
-            LOG.error("Template problem occurred.");
-        }
+        return modelAndView;
     }
 
-    private void errorMessages(Map<String, Object> dataModel) {
-        dataModel.put("body_template", "find-the-closest");
-        dataModel.put("title", "Find the closest charging point");
-        dataModel.put("error", "Please fill the form with correct value");
+    private void errorMessages(ModelAndView modelAndView) {
+        modelAndView.addObject("body_template", "find-the-closest");
+        modelAndView.addObject("title", "Find the closest charging point");
+        modelAndView.addObject("error", "Please fill the form with correct value");
     }
 
     private boolean isStringInRange(String value, int min, int max) {
